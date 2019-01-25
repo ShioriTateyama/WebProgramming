927ccb1df9cee561ff7d1ca907fffdac49e940e7
楯山詩織 <shiori@tatesanshiorinoMacBook-Pro.local>
2019-01-25 14:49:54 +0900

    課題

--- Mock/src/controller/LoginServlet.java
+++ Mock/src/controller/LoginServlet.java
@@ -9,13 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDao;
+import model.User;
+
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -29,18 +32,32 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Login.jsp");
+
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/");
 		dispatcher.forward(request, response);
 	}
+
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		String loginId =request.getParameter(loginId);
+		String password =request.getParameter(password);
+
+		//リクエストパラメータをチェック
+		String errorMsg = "";
+
+		UserDao userDao = new UserDao();
+		User user = userDao.findByLoginInfo(loginId, password);
+
+		if(loginId)
+
 	}
 
 }
